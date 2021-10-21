d1ba7bb237d4048e76fab5464757ee045116ead4
rossa <fashionfish@naver.com>
2021-10-21 15:59:32 +0900

    투두 리스트 part2

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -4,15 +4,29 @@ const toDoList = document.querySelector('.js-toDoList');
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
   const li = document.createElement('li');
   const delBtn = document.createElement('button');
   const span = document.createElement('span');
+  const newId = toDos.length + 1;
   delBtn.innerText = '❌';
   span.innerText = text;
+  li.id = newId;
   li.appendChild(span);
   li.appendChild(delBtn);
   toDoList.appendChild(li);
+  const toDoObj = {
+    id: newId,
+    text: text,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,8 +37,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos === null) {
+  const loadToDos = localStorage.getItem(TODOS_LS);
+  if (loadToDos !== null) {
+    const parsedToDos = JSON.parse(loadToDos);
+    parsedToDos.forEach((toDo) => {
+      paintToDo(toDo.text);
+    });
   }
 }
 
